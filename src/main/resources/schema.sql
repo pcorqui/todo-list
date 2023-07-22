ad292b76cd8bf8b815adda2ea4d53a225c2fc840
paul cortes <paul.cortes@forteinnovation.mx>
2023-07-21 22:55:17 -0600

    UPDATE corrections in data y scheme

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE COURSES{
+CREATE TABLE COURSES(
     id int(15) NOT NULL,
     name varchar(100) NOT NULL,
     category varchar(20) NOT NULL,
     rating int(1) NOT NUll,
-    description varchar (1000) NOT NULL,
+    description varchar(1000) NOT NULL,
     PRIMARY KEY (id)
-}
+);
