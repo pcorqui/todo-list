8f95b560f8870897c057d416dee9f458044fbbd8
paul cortes <paul.cortes@forteinnovation.mx>
2023-07-23 15:39:48 -0600

    add entity courses

---
+++ src/main/java/com/pcorqui/todolist/entity/Course.java
@@ -0,0 +1,82 @@
+package com.pcorqui.todolist.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "COURSES")
+public class Course {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "CATEGORY")
+    private String category;
+
+    @Column(name = "RATING")
+    private int rating;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Course(String name, String category, int rating, String description){
+        this.name = name;
+        this.category = category;
+        this.rating = rating;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", rating=" + rating +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
